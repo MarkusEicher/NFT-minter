71732ad8774c854aaeac0df57f90703306d38aaa
MarkusEicher <markuseicher@outlook.com>
2023-03-30 22:56:50 +0200

    First local commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './MECS-Logo-Icon.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {<img src={logo} className="App-logo" alt="My logo shows a blue circle with a smaller yellow circle in the upper right area. Like an eye looking up at the blue sky with a bright sun." />}
+      </header>
+    </div>
+  );
+}
+
+export default App;
