e7f9e323f50cc83c6db717e858d73af18e5b98b5
MarkusEicher <markuseicher@outlook.com>
2023-03-31 02:36:43 +0200

    Remove h1 title text from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import logo from './MECS-Logo-Icon.png';
-// import './App.css';
+// import React from 'react'; // Import React for using classes
+
 import Header from './components/Header';
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="container">
 
-      <Header />
+      <Header h1title='PoC Whitelisting and NFT minting dApp using a Merkle Tree'/>
 
-      <h1>This is the h1 text from the div with className "container" defined in the App.js</h1>
+      {/* <h1>This is the h1 text from the div with className "container" defined in the App.js</h1> */}
       <h2>A Proof of Concept dApp for whitelisting Ethereum addresses for later minting of an NFT - Project by Markus Eicher inspired by lessons 9 and 10 of the Sophomore Track from LearnWeb3DAOO</h2>
       {/* <p>My name {name} is inserted here from the const name</p> */}
       {/* <p>This is an example of a ternary operator defined as const x it shows yes or no. It's currently set to {ternary1 ? 'Yes' : 'No'}</p> */}
@@ -20,5 +20,13 @@ function App() {
     </div>
   );
 }
+
+// Example of a class component definition
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>This is a hello from a class component</h1>
+//   }
+// }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
+// import logo from './MECS-Logo-Icon.png';
+// import './App.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Whitelisting and NFT minting dApp using a Merkle Tree - From Header.js</h1>
+      <h1>{props.h1title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  h1title: 'Inserted by default props in Header.js'
 }
 
 export default Header
