621283fc5231e0357c4da5590dfabeb88b5bcb1a
MarkusEicher <markuseicher@outlook.com>
2023-04-01 14:05:39 +0200

    Add Favs.js component and update App.js and Header.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import React from 'react'; // Import React for using classes
 // import './App.css';
 import Header from './components/Header';
+import Favs from './components/Favs';
 
-function App() {
+const App = () => {
 
   // const name = "Markus Eicher";
   // const ternary1 = false;
@@ -10,15 +11,14 @@ function App() {
   return (
     <div className="container">
 
-      <Header h1title='PoC Whitelisting and NFT minting dApp using a Merkle Tree'/>
-
+      <Header h1title='PoC Whitelisting and NFT minting dApp using a Merkle Tree' h2description = 'A Proof of Concept dApp for whitelisting Ethereum addresses for later minting of an NFT - Project by Markus Eicher inspired by lessons 9 and 10 of the Sophomore Track from LearnWeb3DAOO'/>
       {/* <h1>This is the h1 text from the div with className "container" defined in the App.js</h1> */}
-      <h2>A Proof of Concept dApp for whitelisting Ethereum addresses for later minting of an NFT - Project by Markus Eicher inspired by lessons 9 and 10 of the Sophomore Track from LearnWeb3DAOO</h2>
+      <Favs />
       {/* <p>My name {name} is inserted here from the const name</p> */}
       {/* <p>This is an example of a ternary operator defined as const x it shows yes or no. It's currently set to {ternary1 ? 'Yes' : 'No'}</p> */}
       
     </div>
-  );
+  )
 }
 
 // Example of a class component definition
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,15 @@ const Header = (props) => {
   return (
     <header className='header'>
       <h1>{props.h1title}</h1>
+      <h2>{props.h2description}</h2>
       <Button color='green' text='Click me'/>
     </header>
   )
 }
 
 Header.defaultProps = {
-  h1title: 'Inserted by default props in Header.js'
+  h1title: 'Inserted by default props in Header.js',
+  h2description: 'Inserted by default props in Header.js'
 }
 
 export default Header
